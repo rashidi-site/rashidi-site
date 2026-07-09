a282a5cd175e92d5960f6fe6068a054081ffec33
Abdul Wahed Rashidi <roarwhd@gmail.com>
2026-07-09 14:10:59 +0600

    fix

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL!,
   import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY!,
